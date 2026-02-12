6b1da38ded07c97c8e1338dd7d270de45b591980
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-02-12 19:51:02 +0100

    chromium: enable widevine, setup preservation

--- home/jacobsen/programs/chromium.nix
+++ home/jacobsen/programs/chromium.nix
@@ -1,4 +1,13 @@
+{ pkgs, ... }:
 {
-  # chromium as fallback browser
-  programs.chromium.enable = true;
+  programs.chromium = {
+    # chromium as fallback browser
+    enable = true;
+    package = pkgs.chromium.override { enableWideVine = true; };
+  };
+
+  # electron apps store their state inseparably in .config/
+  preservation.preserveAt.state-dir.directories = [
+    ".config/chromium"
+  ];
 }

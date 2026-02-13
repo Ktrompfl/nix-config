5df9635db11088a5d1ab297d4ae05606b08d22dc
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-02-13 11:59:10 +0100

    jay: setup realtime scheduling

--- modules/nixos/jay.nix
+++ modules/nixos/jay.nix
@@ -14,6 +14,30 @@ in
 
       package = lib.mkPackageOption pkgs "jay" { };
 
+      realtime-scheduling = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+        description = ''
+          Add CAP_SYS_NICE capabilities to the Jay binary.
+
+          If CAP_SYS_NICE is available, Jay will, by default, elevate its scheduler to SCHED_RR
+          and create Vulkan queues with the highest available priority.
+          This can improve responsiveness if the CPU or GPU are under high load.
+
+          If Jay is started with the environment variable JAY_NO_REALTIME=1 or a config.so exists,
+          then Jay will not elevate its scheduler but will still create elevated Vulkan queues.
+
+          Jay will drop all capabilities almost immediately after being started. Before that,
+          it will spawn a dedicated thread that retains the CAP_SYS_NICE capability to create
+          elevated Vulkan queues later.
+
+          If Jay has elevated its scheduler to SCHED_RR, then it will refuse to load config.so configurations.
+          Otherwise unprivileged applications would be able to run arbitrary code with SCHED_RR by
+          crafting a dedicated config.so. This behavior can be overridden by compiling Jay
+          with JAY_ALLOW_REALTIME_CONFIG_SO=1.
+        '';
+      };
+
       xwayland.enable = lib.mkEnableOption "XWayland" // {
         default = true;
       };
@@ -39,7 +63,8 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [ cfg.package ] ++ cfg.extraPackages;
+    environment.systemPackages =
+      (lib.optional (!cfg.realtime-scheduling) cfg.package) ++ cfg.extraPackages;
 
     programs = {
       dconf.enable = lib.mkDefault true;
@@ -49,6 +74,15 @@ in
     security = {
       polkit.enable = true;
       pam.services.swaylock = { };
+
+      wrappers = lib.mkIf cfg.realtime-scheduling {
+        jay = {
+          owner = "root";
+          group = "root";
+          source = "${lib.getExe cfg.package}";
+          capabilities = "cap_sys_nice+p";
+        };
+      };
     };
 
     xdg.portal = {

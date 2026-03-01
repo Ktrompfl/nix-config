04ce9632e8ff8406c1d95a949ef3042978625c40
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-03-01 19:32:00 +0100

    steam: setup global environment

--- system/programs/steam.nix
+++ system/programs/steam.nix
@@ -4,5 +4,14 @@
     remotePlay.openFirewall = true; # open ports in the firewall for steam remote play
     dedicatedServer.openFirewall = true; # open ports in the firewall for source dedicated server
     extraCompatPackages = [ pkgs.proton-ge-bin ]; # add proton ge
+    package = pkgs.steam.override {
+      extraEnv = {
+        MANGOHUD = "1";
+        # GAMEMODERUN = "1";
+        PROTON_ENABLE_WAYLAND = "1";
+        PROTON_ENABLE_HDR = "1";
+        PROTON_FSR4_RDNA3_UPGRADE = "1";
+      };
+    };
   };
 }

a6f734d3fc6efd92a21b889ff579349960fe7a28
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-06-09 12:01:11 +0200

    ssh: configure hosts

--- home/jacobsen/ssh.nix
+++ home/jacobsen/ssh.nix
@@ -1,4 +1,56 @@
 {
+  programs.ssh = {
+    enable = true;
+    settings = {
+      "*" = {
+        ForwardAgent = false;
+        AddKeysToAgent = "no";
+        Compression = false;
+        ServerAliveInterval = 15;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        UserKnownHostsFile = "~/.ssh/known_hosts";
+        ControlMaster = "auto";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ControlPersist = "5m";
+      };
+      fsmathe = {
+        HostName = "fsmathe.mathematik.uni-kl.de";
+        User = "jacobsen";
+      };
+      linda = {
+        HostName = "linda.rhrk.uni-kl.de";
+        User = "jacobsen";
+      };
+      lindb = {
+        HostName = "lindb.rhrk.uni-kl.de";
+        User = "jacobsen";
+      };
+      skylla = {
+        HostName = "skylla.mathematik.uni-kl.de";
+        User = "jacobsen";
+      };
+    }
+    // builtins.listToAttrs (
+      map
+        (name: {
+          inherit name;
+          value = {
+            User = "jacobsen";
+            HostName = "${name}.math.rptu.de";
+            ProxyJump = "skylla";
+          };
+        })
+        [
+          "cipserv01"
+          "cipserv02"
+          "cipserv03"
+          "cipserv04"
+          "cipserv05"
+        ]
+    );
+  };
+
   # persist ssh keys / known hosts
   preservation.preserveAt.data-dir.directories = [
     {

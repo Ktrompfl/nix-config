ebea7c8fff503d64a3b3b6a75ad476f614341cfb
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-07-19 20:31:13 +0200

    hallandren: setup secondary disk

--- hosts/hallandren/filesystem/disks.nix
+++ hosts/hallandren/filesystem/disks.nix
@@ -1,89 +1,138 @@
 { inputs, ... }:
 let
-  disk0 = "/dev/nvme0n1";
+  disk0 = "/dev/disk/by-id/nvme-Samsung_SSD_980_1TB_S649NU0W832448H";
+  disk1 = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0N505840E";
   boot-size = "1G";
   swap-size = "72G";
 in
 {
   imports = [ inputs.disko.nixosModules.disko ];
 
-  disko.devices.disk.disk0 = {
-    type = "disk";
-    device = disk0;
-    content = {
-      type = "gpt";
-      partitions = {
-        ESP = {
-          size = boot-size;
-          type = "EF00";
-          content = {
-            type = "filesystem";
-            format = "vfat";
-            mountpoint = "/boot";
-            mountOptions = [
-              "nofail"
-              "umask=0077"
-            ];
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        device = disk0;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = boot-size;
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "nofail"
+                  "umask=0077"
+                ];
+              };
+            };
+
+            crypt0 = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypt0";
+                settings.allowDiscards = true;
+                content = {
+                  type = "lvm_pv";
+                  vg = "vg0";
+                };
+              };
+            };
           };
         };
-
-        crypt0 = {
-          size = "100%";
-          content = {
-            type = "luks";
-            name = "crypt0";
-            settings.allowDiscards = true;
-            content = {
-              type = "lvm_pv";
-              vg = "vg0";
+      };
+      disk1 = {
+        type = "disk";
+        device = disk1;
+        content = {
+          type = "gpt";
+          partitions = {
+            crypt1 = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypt1";
+                settings.allowDiscards = true;
+                content = {
+                  type = "lvm_pv";
+                  vg = "vg1";
+                };
+              };
             };
           };
         };
       };
     };
-  };
 
-  disko.devices.lvm_vg.vg0 = {
-    type = "lvm_vg";
-    lvs = {
-      swap = {
-        size = swap-size;
-        content = {
-          type = "swap";
-          resumeDevice = true;
-          discardPolicy = "both";
+    lvm_vg = {
+      vg0 = {
+        type = "lvm_vg";
+        lvs = {
+          swap = {
+            size = swap-size;
+            content = {
+              type = "swap";
+              resumeDevice = true;
+              discardPolicy = "both";
+            };
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes =
+                let
+                  commonOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                in
+                {
+                  "/nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = commonOptions;
+                  };
+                  "/cache" = {
+                    mountpoint = "/cache";
+                    mountOptions = commonOptions;
+                  };
+                  "/persist" = {
+                    mountpoint = "/persist";
+                    mountOptions = commonOptions;
+                  };
+                  "/log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = commonOptions;
+                  };
+                };
+            };
+          };
         };
       };
-      root = {
-        size = "100%";
-        content = {
-          type = "btrfs";
-          extraArgs = [ "-f" ];
-          subvolumes =
-            let
-              commonOptions = [
-                "compress=zstd"
-                "noatime"
-              ];
-            in
-            {
-              "/nix" = {
-                mountpoint = "/nix";
-                mountOptions = commonOptions;
-              };
-              "/cache" = {
-                mountpoint = "/cache";
-                mountOptions = commonOptions;
-              };
-              "/persist" = {
-                mountpoint = "/persist";
-                mountOptions = commonOptions;
-              };
-              "/log" = {
-                mountpoint = "/var/log";
-                mountOptions = commonOptions;
+      vg1 = {
+        type = "lvm_vg";
+        lvs = {
+          extra = {
+            size = "100%";
+            content = {
+              type = "btrfs";
+              extraArgs = [ "-f" ];
+              subvolumes = {
+                "/extra" = {
+                  mountpoint = "/extra";
+                  mountOptions = [
+                    "compress=zstd"
+                    "noatime"
+                  ];
+                };
               };
             };
+          };
         };
       };
     };

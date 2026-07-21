5ca8f7f95fb0e3e60ac2e30808b668de3c0f06c2
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-07-21 11:38:51 +0200

    pipewire: add rnnoise for noise suppression

--- system/services/pipewire.nix
+++ system/services/pipewire.nix
@@ -1,4 +1,9 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 let
   cfg = config.services.pipewire;
 in
@@ -11,6 +16,43 @@ lib.mkIf cfg.enable {
     alsa.support32Bit = lib.mkDefault true;
     pulse.enable = lib.mkDefault true;
     wireplumber.enable = lib.mkDefault true;
+
+    # noise suppression
+    extraLadspaPackages = [ pkgs.rnnoise-plugin ];
+    extraConfig.pipewire."99-input-denoising" = {
+      "context.modules" = [
+        {
+          name = "libpipewire-module-filter-chain";
+          args = {
+            "node.description" = "Noise Canceling source";
+            "media.name" = "Noise Canceling source";
+            "filter.graph" = {
+              nodes = [
+                {
+                  type = "ladspa";
+                  name = "rnnoise";
+                  plugin = "librnnoise_ladspa";
+                  label = "noise_suppressor_stereo";
+                  control = {
+                    "VAD Threshold (%)" = 95.0;
+                    "VAD Grace Period (ms)" = 200;
+                    "Retroactive VAD Grace (ms)" = 0;
+                  };
+                }
+              ];
+            };
+            "capture.props" = {
+              "node.passive" = true;
+              "audio.rate" = 48000;
+            };
+            "playback.props" = {
+              "media.class" = "Audio/Source";
+              "audio.rate" = 48000;
+            };
+          };
+        }
+      ];
+    };
   };
 
   users.users.jacobsen.extraGroups = [

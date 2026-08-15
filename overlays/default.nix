50d844a8fbbd19f0b077cb7e1e76b4ea5de0d4de
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-08-15 17:11:04 +0200

    moonlight-qt: overlay with unstable version from master

--- overlays/default.nix
+++ overlays/default.nix
@@ -12,8 +12,25 @@
   # You can change versions, add patches, set compilation flags, anything really.
   # https://nixos.wiki/wiki/Overlays
   modifications = final: prev: {
-    # example = prev.example.overrideAttrs (oldAttrs: rec {
-    # ...
-    # });
+    # moonlight-qt 6.1.0 predates upstream's ffmpeg 7.1 API migration and no longer builds
+    # against current ffmpeg. Follow master until 6.2.0 releases, as in
+    # https://github.com/NixOS/nixpkgs/pull/552544
+    moonlight-qt = prev.moonlight-qt.overrideAttrs (oldAttrs: {
+      version = "6.1.0-unstable-2026-08-06";
+
+      src = final.fetchFromGitHub {
+        owner = "moonlight-stream";
+        repo = "moonlight-qt";
+        rev = "2e13ed9977bc31c73caf8428f08f58d793313ece";
+        hash = "sha256-kCm/YoFGcXhF/Abi5lRV5F7H1AbKJchdDOlfBVR0tRA=";
+        fetchSubmodules = true;
+      };
+
+      # the Xcode < 14 fix is already in master
+      patches = [ ];
+
+      # would point at a tag that doesn't exist
+      meta = removeAttrs oldAttrs.meta [ "changelog" ];
+    });
   };
 }

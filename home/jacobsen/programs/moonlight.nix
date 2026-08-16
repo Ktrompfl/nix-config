36273c76f378886a66ffc2bb7d206785f2cce3f5
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-08-16 18:36:52 +0200

    flake: update

--- home/jacobsen/programs/moonlight.nix
+++ home/jacobsen/programs/moonlight.nix
@@ -3,6 +3,6 @@
   home.packages = [ pkgs.moonlight-qt ]; # remote play
 
   preservation.preserveAt.state-dir.directories = [
-    ".config/Moonlight Game Streaming Project/Moonlight.conf"
+    ".config/Moonlight Game Streaming Project"
   ];
 }

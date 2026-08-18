d90a25afe04585f234493fcefa8750d72be06795
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-08-18 13:30:35 +0200

    ninjabrain-bot-xwayland: add desktop entry

--- pkgs/ninjabrain-bot-xwayland/default.nix
+++ pkgs/ninjabrain-bot-xwayland/default.nix
@@ -1,5 +1,7 @@
 {
+  imagemagick,
   lib,
+  makeDesktopItem,
   makeWrapper,
   ninjabrain-bot,
   python3Packages,
@@ -14,10 +16,31 @@ let
     libraries = [ python3Packages.xlib ];
     flakeIgnore = [ "E501" ];
   } (builtins.readFile ./main.py);
+
+  desktopItem = makeDesktopItem {
+    name = "ninjabrain-bot-xwayland";
+    desktopName = "Ninjabrain Bot (Xwayland)";
+    comment = "Stronghold calculator for Minecraft speedrunning";
+    exec = "ninjabrain-bot-xwayland";
+    icon = "ninjabrain-bot-xwayland";
+    categories = [
+      "Game"
+      "Utility"
+    ];
+    keywords = [
+      "minecraft"
+      "speedrun"
+      "stronghold"
+      "mcsr"
+    ];
+  };
 in
 runCommand "ninjabrain-bot-xwayland"
   {
-    nativeBuildInputs = [ makeWrapper ];
+    nativeBuildInputs = [
+      imagemagick
+      makeWrapper
+    ];
 
     meta = {
       description = "Ninjabrain Bot (Xwayland)";
@@ -35,4 +58,16 @@ runCommand "ninjabrain-bot-xwayland"
           xwayland
         ]
       }
+
+    install -Dm644 ${desktopItem}/share/applications/ninjabrain-bot-xwayland.desktop \
+      $out/share/applications/ninjabrain-bot-xwayland.desktop
+
+    # The bot ships one 640x640 icon, a size hicolor does not declare, so
+    # nothing looking an icon up the documented way ever finds it.
+    for size in 48 64 128 256 512; do
+      dir=$out/share/icons/hicolor/''${size}x''${size}/apps
+      mkdir -p $dir
+      magick ${ninjabrain-bot}/share/icons/hicolor/640x640/apps/ninjabrain-bot.png \
+        -resize ''${size}x''${size} $dir/ninjabrain-bot-xwayland.png
+    done
   ''

a65942892b72daa6c1787059f265d9b4c1118ed2
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-08-26 13:28:33 +0200

    plymouth: fix boot sequence

--- system/boot/default.nix
+++ system/boot/default.nix
@@ -2,4 +2,7 @@
   imports = [
     ./plymouth.nix
   ];
+
+  # longer timeout before luks decryption fails and drops into emergency shell
+  boot.initrd.systemd.settings.Manager.DefaultDeviceTimeoutSec = "5min";
 }
--- system/boot/plymouth.nix
+++ system/boot/plymouth.nix
@@ -8,5 +8,14 @@ in
     "splash"
   ];
 
+  boot.initrd.systemd = lib.mkIf cfg.enable {
+    # start plymouth after amdgpu is loaded in initrd
+    services.plymouth-start.after = [ "systemd-modules-load.service" ];
+
+    # fallback to systemd to prompt for passphrase when plymouth fails
+    services.systemd-ask-password-console.unitConfig.ConditionPathExists = "";
+    paths.systemd-ask-password-console.unitConfig.ConditionPathExists = "";
+  };
+
   preservation.preserveAt.state-dir.directories = lib.optional cfg.enable "/var/lib/plymouth";
 }

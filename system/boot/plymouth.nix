3b3d77df95f910bb585e554bea71e91b6c07ab2c
Nicolaus Jacobsen <jacobsen@rptu.de>
2026-08-26 16:07:40 +0200

    plymouth: systemd-ask-password can't run at the same time or keyboard inputs are intercepted

--- system/boot/plymouth.nix
+++ system/boot/plymouth.nix
@@ -11,10 +11,6 @@ in
   boot.initrd.systemd = lib.mkIf cfg.enable {
     # start plymouth after amdgpu is loaded in initrd
     services.plymouth-start.after = [ "systemd-modules-load.service" ];
-
-    # fallback to systemd to prompt for passphrase when plymouth fails
-    services.systemd-ask-password-console.unitConfig.ConditionPathExists = "";
-    paths.systemd-ask-password-console.unitConfig.ConditionPathExists = "";
   };
 
   preservation.preserveAt.state-dir.directories = lib.optional cfg.enable {
